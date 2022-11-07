8f7d7b79c614aa0a9936e3941d37192c867b8d4c
Noahkimtai <noahkimtai24@gmail.com>
2022-11-07 20:05:36 +0300

    aggregate and associate methods for user created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
-    has_many :products, through: :reviews    
+    has_many :products, through: :reviews
+    
+    def favorite_product
+        max_rating = self.reviews.order(:star_rating).last
+        max_rating.product
+    end
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
 end
